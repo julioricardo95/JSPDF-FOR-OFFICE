4f9d9f44896071f7cd36b11d6846c87c55c2facb
julioricardo95 <julioricardo95@gmail.com>
2024-11-29 12:14:08 -0600

    gp
    gp

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://julioricardo95.github.io',
+  base: '/JSPDF-FOR-OFFICE',
 });
